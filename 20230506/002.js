13bfd496301a258ad83431eec5fffcf4a7b55666
an1413 <87430624+an1413@users.noreply.github.com>
2023-05-07 23:56:24 +0900

    TIL

--- 20230506/002.js
+++ 20230506/002.js
@@ -67,3 +67,199 @@ hjage
 .catch(() => {
     console.log("효정이는 미성년자~!");
 })
+
+
+function whatage(age) {
+    return new Promise((resolve, reject) => {
+        if(age > 20) resolve();
+        else reject();
+    })
+}
+
+const nastar = whatage(21);
+
+nastar
+.then(() => {
+    console.log('성인');
+}).catch(() => {
+    console.log('미성년');
+})
+
+
+function howlike(item) {
+    return new Promise((resolve, reject) => {
+        if(item === 'apple') resolve();
+        else reject();
+    })
+}
+
+const item1 = howlike(banana);
+const item2 = howlike('apple');
+
+item1
+    .then(() => {
+        console.log(`굿~!`);
+    })
+    .catch(console.log('별로'));
+
+item2
+    .then(() => {
+        console.log(`${item2}은 굿~!`);
+    }).catch (() => {
+console.log('ㅋ.ㅋ');
+    }) 
+
+function setTimeoutPromise(delay) {
+    return new Promise((resolve) => setTimteout(resolve, delay));
+}
+
+async function startAsync() {
+    await setTimeoutPromise(1000);
+    console.log("1초 지났습니다.");
+}
+
+console.log("시작입니다.");
+
+startAsync();
+
+async function f() {
+    let promise = new Promise((resolve,reject) => {
+        setTimeout(() => resolve("완료!"), 1000)
+    })
+
+    let result = await promise;
+
+    alert(result);
+}
+
+f();
+
+
+// function loadjson(url) {
+//     return fetch(url)
+//         .then(response => {
+//             if(response.status == 200){
+//                 return response.json();
+//             }
+//             else {
+//                 throw new Error(response.status);
+//             }
+//         })
+// }
+
+// loadjson('no-such-user.json')
+//     .catch(alert); // Error: 404
+
+
+// function loadjson(url) {
+//     return fetch(url)
+//         .then(response => {
+//             if(response.status == 200){
+//                 return response.json();
+//             } else {
+//                 throw new Error(response.status);
+//             }
+//         })
+// }
+
+// loadjson('no-such-user.json')
+//     .catch(alert) // Error : 404
+
+// function loadjson(url) {
+//     return fetch(url)
+//         .then(response => {
+//             if(response.status == 200) {
+//                 return response.json();
+//             } else {
+//                 throw new Error(response.status);
+//             }
+//         })
+// }
+
+// loadjson('no-no')
+//     .catch(alert); 
+
+// async function loadjson(url) {
+//     return   await fetch(url) (response => {
+//             if(response.status == 200) {
+//                 return response.json();
+//             } else {
+//                 throw new Error(response.status);
+//             }
+//         })
+// }
+
+
+// loadjson('nononono')
+//     .catch(alert);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function loadjson(url) {
+    let response = await fetch(url);
+
+    if(response.status == 200) {
+        let json = await response.json();
+        return json;
+    }
+
+    throw new Error(response.status);
+        
+}
+
+loadjson('nonono')
+    .catch(alert);
+
+
+async function loadjson(url) {
+    let response = await fetch(url)
+
+    if(response.status===200) {
+        let json = await response.json();
+        return json
+    }
+    throw new Error(response.status);
+    
+}
+
+loadjson('nonono')
+    .catch(alert);
+
+
+async function lloadjson(url) {
+
+}
+
+
+async function wait() {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  return 10;
+}
+
+function f() {
+    return wait().then(result => {
+    console.log(result); // 10
+  });
+
+}
+
+f();
